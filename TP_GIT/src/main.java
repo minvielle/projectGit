033cd39ba932b496a73a8ae750fb4548881f0346
Matthieu <minvielle.ma@gmail.com>
2014-03-25 17:19:11 +0100

    Correction apporté

--- TP_GIT/src/main.java
+++ TP_GIT/src/main.java
@@ -6,7 +6,7 @@ public class main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Veuillez entrer votre age ? ");
-	    System.out.println("Tu as "+sc.next()+ " ans!!");		
+	    System.out.println("Tu as "+sc.nextInt()+ " ans!!");		
 		sc.close();
 
 	}
